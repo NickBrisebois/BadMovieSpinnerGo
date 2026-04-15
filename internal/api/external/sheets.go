0f01b798554594482e24b77b45c35f2a462aaf82
NickBrisebois <nickbrisebois12@gmail.com>
2026-04-15 09:29:34 -0400

    feat: setup google sheets handlers

--- internal/api/external/sheets.go
+++ internal/api/external/sheets.go
@@ -2,6 +2,7 @@ package external
 
 import (
 	"context"
+	"fmt"
 
 	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
@@ -9,10 +10,12 @@ import (
 
 type GoogleSheetsAPI struct {
 	sheetsService *sheets.Service
+	spreadsheetID string
+	rawGoogleDoc  *sheets.Spreadsheet
 }
 
-func NewGoogleSheetsAPI(credentialsFile, spreadsheetID string) (*GoogleSheetsAPI, error) {
-	sheetsClientOptions := option.WithAuthCredentialsFile(option.ServiceAccount, credentialsFile)
+func NewGoogleSheetsAPI(credentialsFilePath, spreadsheetID string) (*GoogleSheetsAPI, error) {
+	sheetsClientOptions := option.WithAuthCredentialsFile(option.ServiceAccount, credentialsFilePath)
 	sheetsService, err := sheets.NewService(
 		context.Background(),
 		sheetsClientOptions,
@@ -21,5 +24,32 @@ func NewGoogleSheetsAPI(credentialsFile, spreadsheetID string) (*GoogleSheetsAPI
 	if err != nil {
 		return nil, err
 	}
-	return &GoogleSheetsAPI{sheetsService: sheetsService}, nil
+
+	spreadsheet, err := sheetsService.Spreadsheets.Get(spreadsheetID).Do()
+	if err != nil {
+		return nil, err
+	}
+	return &GoogleSheetsAPI{sheetsService: sheetsService, spreadsheetID: spreadsheetID, rawGoogleDoc: spreadsheet}, nil
+}
+
+func (g *GoogleSheetsAPI) GetMovies() (*[]string, error) {
+	if len(g.rawGoogleDoc.Sheets) == 0 {
+		return nil, nil
+	}
+
+	mainSheetName := g.rawGoogleDoc.Sheets[0].Properties.Title
+	readRange := fmt.Sprintf("%s!A:Z", mainSheetName)
+
+	movieListValues, err := g.sheetsService.Spreadsheets.Values.Get(g.spreadsheetID, readRange).Do()
+	if err != nil {
+		return nil, nil
+	}
+
+	movies := make([]string, 0, len(movieListValues.Values))
+	for _, row := range movieListValues.Values {
+		if len(row) > 0 {
+			movies = append(movies, row[0].(string))
+		}
+	}
+	return &movies, nil
 }
--- internal/api/handlers/sheets.go
+++ internal/api/handlers/sheets.go
@@ -1,1 +1,24 @@
 package handlers
+
+import "NickBrisebois/BadMovieSpinnerGo/internal/api/external"
+
+type GSheetsHandler struct {
+	sheetsAPI *external.GoogleSheetsAPI
+}
+
+func NewGSheetsHandler(credentialsFilePath, spreadsheetID string) (*GSheetsHandler, error) {
+	handler := &GSheetsHandler{sheetsAPI: nil}
+	sheetsAPI, err := external.NewGoogleSheetsAPI(
+		credentialsFilePath,
+		spreadsheetID,
+	)
+	if err != nil {
+		return nil, err
+	}
+	handler.sheetsAPI = sheetsAPI
+	return handler, nil
+}
+
+func (h *GSheetsHandler) GetMovieList() {
+
+}
