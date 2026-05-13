d84e6d9003d87219b6105ad90fd08290e817ae66
NickBrisebois <nickbrisebois12@gmail.com>
2026-05-13 17:34:03 -0400

    feat: clean up stylesheet

--- internal/web/html/static/game.js
+++ internal/web/html/static/game.js
@@ -3,24 +3,3 @@ const go = new Go();
 WebAssembly.instantiateStreaming(fetch("static/spinner.wasm"), go.importObject).then((result) => {
   go.run(result.instance);
 });
-
-document.addEventListener("DOMContentLoaded", () => {
-  const preventScrollOnCanvas = () => {
-    const spinnerCanvas = document.querySelector("canvas");
-    if (spinnerCanvas) {
-      console.log("preventing ebitengine from taking scrollwheel events");
-      spinnerCanvas.addEventListener(
-        "wheel",
-        (e) => {
-          e.preventDefault();
-          e.stopPropagation();
-        },
-        { capture: true, passing: false },
-      );
-    } else {
-      setTimeout(preventScrollOnCanvas, 100);
-    }
-  };
-
-  requestAnimationFrame(() => preventScrollOnCanvas());
-});
